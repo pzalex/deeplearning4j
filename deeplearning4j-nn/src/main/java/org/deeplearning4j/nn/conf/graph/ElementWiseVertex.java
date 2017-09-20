5555015aa94e26a6d7361b259c14d8fd9f3621f5
Alex Black <blacka101@gmail.com>
2017-09-20 14:38:09 +1000

    Preprocessor fixes; InputType multi in/out support

--- deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/conf/graph/ElementWiseVertex.java
+++ deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/conf/graph/ElementWiseVertex.java
@@ -116,9 +116,9 @@ public class ElementWiseVertex extends GraphVertex {
     }
 
     @Override
-    public InputType getOutputType(int layerIndex, InputType... vertexInputs) throws InvalidInputTypeException {
+    public InputType[] getOutputType(int layerIndex, InputType... vertexInputs) throws InvalidInputTypeException {
         if (vertexInputs.length == 1)
-            return vertexInputs[0];
+            return vertexInputs;
         InputType first = vertexInputs[0];
         if (first.getType() != InputType.Type.CNN) {
             //FF, RNN or flat CNN data inputs
@@ -159,7 +159,7 @@ public class ElementWiseVertex extends GraphVertex {
                 }
             }
         }
-        return first; //Same output shape/size as
+        return new InputType[]{first}; //Same output shape/size as
     }
 
     @Override
