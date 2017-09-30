0ddf37e7a0533e37000ed7017f15684b1919e98c
Alex Black <blacka101@gmail.com>
2017-09-30 17:40:22 +1000

    More NeuralNetConfiguration refactoring

--- deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/conf/graph/ElementWiseVertex.java
+++ deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/conf/graph/ElementWiseVertex.java
@@ -79,8 +79,7 @@ public class ElementWiseVertex extends BaseGraphVertex {
     }
 
     @Override
-    public Layer instantiate(NeuralNetConfiguration conf,
-                             Collection<IterationListener> iterationListeners,
+    public Layer instantiate(Collection<IterationListener> iterationListeners,
                              String name, int idx, int numInputs, INDArray layerParamsView,
                              boolean initializeParams) {
         org.deeplearning4j.nn.graph.vertex.impl.ElementWiseVertex.Op op;
